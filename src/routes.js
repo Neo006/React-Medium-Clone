d86913ee313882512b594421a264092abbd3bdee
Neo006 <vahe95vahe@gmail.com>
2022-01-22 21:01:43 +0400

    add your feed

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ import GlobalFeed from './pages/globalFeed';
 import Authentication from './pages/authentication';
 import Article from './pages/article';
 import TagFeed from './pages/tagFeed';
+import YourFeed from './pages/yourFeed';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<GlobalFeed />} />
+      <Route path="/feed" element={<YourFeed />} />
       <Route path="/tags/:slug" element={<TagFeed />} />
       <Route path="/login" element={<Authentication />} />
       <Route path="/register" element={<Authentication />} />
