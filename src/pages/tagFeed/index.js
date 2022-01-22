94c2e5839476bd59034ef7c9551f03f3bdf885e1
Neo006 <vahe95vahe@gmail.com>
2022-01-22 16:22:41 +0400

    add tag feed

---
+++ src/pages/tagFeed/index.js
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react';
+import Feed from '../../components/feed';
+import Pagination from '../../components/pagination';
+import useFetch from '../../hooks/useFetch';
+import { getPaginator } from '../../utils';
+import { useLocation, useParams } from 'react-router-dom';
+import { stringify } from 'query-string';
+import { limit } from '../../utils';
+import PopularTags from '../../components/popularTags';
+import Loading from '../../components/loading';
+import ErrorMessage from '../../components/errorMessage';
+import FeedToggler from '../../components/feedToggler';
+
+const TagFeed = () => {
+  const { slug } = useParams();
+  const location = useLocation();
+  const { currentPage, offset } = getPaginator(location.search);
+  const stringifiedParams = stringify({
+    limit,
+    offset,
+    tag: slug,
+  });
+  const apiUrl = `/articles?${stringifiedParams}`;
+  const [{ response, isLoading, error }, doFetch] = useFetch(apiUrl);
+
+  useEffect(() => {
+    doFetch();
+  }, [doFetch, currentPage, slug]);
+
+  return (
+    <div className="home-page">
+      <div className="banner">
+        <div className="container">
+          <h1>Medium Clone</h1>
+          <p>A place to share knowledge</p>
+        </div>
+      </div>
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-9">
+            <FeedToggler tagName={slug} />
+            {isLoading && <Loading />}
+            {error && <ErrorMessage />}
+            {!isLoading && response && (
+              <>
+                <Feed articles={response.articles} />
+                {response.articlesCount > limit && (
+                  <Pagination
+                    total={response.articlesCount}
+                    limit={limit}
+                    url={location.pathname}
+                    currentPage={currentPage}
+                  />
+                )}
+              </>
+            )}
+          </div>
+          <div className="col-md-3">
+            <PopularTags />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TagFeed;
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import {Routes, Route} from 'react-router-dom';
-import GlobalFeed from './pages/globalFeed'
+import { Routes, Route } from 'react-router-dom';
+import GlobalFeed from './pages/globalFeed';
 import Authentication from './pages/authentication';
 import Article from './pages/article';
+import TagFeed from './pages/tagFeed';
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<GlobalFeed/>} />
-      <Route path="/login" element={<Authentication/>} />
+      <Route path="/" element={<GlobalFeed />} />
+      <Route path="/tags/:slug" element={<TagFeed />} />
+      <Route path="/login" element={<Authentication />} />
       <Route path="/register" element={<Authentication />} />
-      <Route path="/articles/:slug" element={<Article/>} />
+      <Route path="/articles/:slug" element={<Article />} />
     </Routes>
-  )
-}
+  );
+};
 
 export default AppRoutes;
