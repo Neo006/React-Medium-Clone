bf9e27a3aa44a244f69704fba6e5a0d4cfc86472
@vahe1995 <vahe.ghonakhchyan@innline.am>
2022-01-24 12:36:44 +0400

    add new post form

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,13 @@ import Authentication from './pages/authentication';
 import Article from './pages/article';
 import TagFeed from './pages/tagFeed';
 import YourFeed from './pages/yourFeed';
+import CreateArticle from './pages/createArticle';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<GlobalFeed />} />
+      <Route path="/articles/new" element={<CreateArticle />} />
       <Route path="/feed" element={<YourFeed />} />
       <Route path="/tags/:slug" element={<TagFeed />} />
       <Route path="/login" element={<Authentication />} />
