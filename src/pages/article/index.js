19604859acc1b5dbbfda5dcc6ecf180dd638c939
@vahe1995 <vahe.ghonakhchyan@innline.am>
2022-02-03 17:10:15 +0400

    add follow post author functional

--- src/pages/article/index.js
+++ src/pages/article/index.js
@@ -1,20 +1,33 @@
-import React, { useEffect, useContext, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import useFetch from '../../hooks/useFetch';
-import { Link, Navigate } from 'react-router-dom';
-import Loading from '../../components/loading';
-import ErrorMessage from '../../components/errorMessage';
-import TagList from '../../components/tagList';
-import { CurrentUserContext } from '../../contexts/currentUser';
+import React, { useEffect, useContext, useState } from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import { Link, Navigate } from "react-router-dom";
+import Loading from "../../components/loading";
+import ErrorMessage from "../../components/errorMessage";
+import TagList from "../../components/tagList";
+import { CurrentUserContext } from "../../contexts/currentUser";
+import classNames from "classnames";
 
 const Article = () => {
   const { slug } = useParams();
   const apiUrl = `/articles/${slug}`;
-  const [{ response: fetchArticleResponse, isLoading: fetchArticleIsLoading, error: fetchArticleError }, doFetch] =
-    useFetch(apiUrl);
+  const [{ response: fetchArticleResponse, isLoading: fetchArticleIsLoading, error: fetchArticleError }, doFetch] = useFetch(apiUrl);
   const [{ responseStatus: deleteArticleResponseStatus }, doDeleteArticle] = useFetch(apiUrl);
   const [currenUserState] = useContext(CurrentUserContext);
   const [isSuccessfullDelete, setIsSuccessfullDelete] = useState(false);
+
+  const authorUsername = fetchArticleResponse ? fetchArticleResponse.article.author.username : "";
+  const followApiUrl = `/profiles/${authorUsername}/follow`;
+  const [{ response: followResponse }, doFollow] = useFetch(followApiUrl);
+  const [isFollowingAuthor, setIsFollowingAuthor] = useState("");
+  const followButtonText = isFollowingAuthor ? "Unfollow" : "Follow";
+  const followButtonClasses = classNames({
+    btn: true,
+    "btn-sm": true,
+    "action-btn": true,
+    "btn-secondary": isFollowingAuthor,
+    "btn-outline-secondary": !isFollowingAuthor,
+  });
 
   const isAuthor = () => {
     if (!fetchArticleResponse || !currenUserState.isLoggedIn) {
@@ -25,9 +38,29 @@ const Article = () => {
 
   const deleteArticle = () => {
     doDeleteArticle({
-      method: 'delete',
+      method: "delete",
     });
   };
+
+  const followAuthor = () => {
+    doFollow({
+      method: isFollowingAuthor ? "delete" : "post",
+    });
+  };
+
+  useEffect(() => {
+    if (!fetchArticleResponse) {
+      return;
+    }
+    setIsFollowingAuthor(fetchArticleResponse.article.author.following);
+  }, [fetchArticleResponse]);
+
+  useEffect(() => {
+    if (!followResponse) {
+      return;
+    }
+    setIsFollowingAuthor(followResponse.profile.following);
+  }, [followResponse]);
 
   useEffect(() => {
     doFetch();
@@ -51,28 +84,46 @@ const Article = () => {
           <div className="container">
             <h1>{fetchArticleResponse.article.title}</h1>
             <div className="article-meta">
-              <Link to={`/profiles/${fetchArticleResponse.article.author.username}`}>
+              <Link
+                to={`/profiles/${fetchArticleResponse.article.author.username}`}
+              >
                 <img src={fetchArticleResponse.article.author.image} alt="" />
               </Link>
               <div className="info">
-                <Link to={`/profiles/${fetchArticleResponse.article.author.username}`}>
+                <Link
+                  to={`/profiles/${fetchArticleResponse.article.author.username}`}
+                >
                   {fetchArticleResponse.article.author.username}
                 </Link>
-                <span className="date">{fetchArticleResponse.article.createdAt}</span>
+                <span className="date">
+                  {fetchArticleResponse.article.createdAt}
+                </span>
               </div>
               {isAuthor() && (
                 <span>
                   <Link
                     className="btn btn-outline-secondary btn-sm"
-                    style={{ marginRight: '10px' }}
+                    style={{ marginRight: "10px" }}
                     to={`/articles/${fetchArticleResponse.article.slug}/edit`}
                   >
                     <i className="ion-edit"></i>
-                    Edit Article
+                    &nbsp; Edit Article
                   </Link>
-                  <button className="btn btn-outline-danger btn-sm" onClick={deleteArticle}>
+                  <button
+                    className="btn btn-outline-danger btn-sm"
+                    onClick={deleteArticle}
+                  >
                     <i className="ion-trash-a"></i>
-                    Delete Article
+                    &nbsp; Delete Article
+                  </button>
+                </span>
+              )}
+              {!isAuthor() && (
+                <span>
+                  <button className={followButtonClasses} onClick={followAuthor}>
+                    <i className="ion-plus-round"></i>
+                    &nbsp; {followButtonText}
+                    {fetchArticleResponse.article.author.username}
                   </button>
                 </span>
               )}
